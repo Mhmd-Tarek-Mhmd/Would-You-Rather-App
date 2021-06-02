9233cbfd39e05deee3e2bf4fbb2e8f3fb584115a
Mhmd <biboo.biboo97@gmail.com>
2021-06-02 22:25:01 +0200

    adding-fake-data & creating-store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import 'normalize.css'
+import './index.css'
+import App from './App'
+
+import reducer from './reducers'
+import middleware from './middleware'
+import { handleInitialData } from './actions/shared'
+
+
+const store = createStore(reducer, middleware)
+store.dispatch(handleInitialData())
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
